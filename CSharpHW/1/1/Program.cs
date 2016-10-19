dd8a57d072d65edf2bd5e4486b4f92db5c2ba927
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-10-19 21:15:20 +0300

    Init

---
+++ CSharpHW/1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello, world");
+            Console.ReadKey();
+        }
+    }
+}
